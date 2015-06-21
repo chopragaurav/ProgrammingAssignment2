c85b8231e8703addef93bfa2b04203de811e8a7d
chopradimpy@gmail.com <chopradimpy@gmail.com>
2015-06-21 13:09:48 +0530

    Submitting my assignmentno_2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions listed below (a) makeCacheMatrix (b) cacheSolve
+## Both these functions are used to find inverse of invertible matrix and put output in a cache 
 
-## Write a short comment describing this function
+## This function returns a list of objects containing matrixes and put them in a cache
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+	m <- NULL
+    set <- function(y) {
+            x <<- y
+            m <<- NULL
+    }
+	checkIfNotInvertible <- function(X)
+	{
+		out <- tryCatch(solve(X) %*% X, error = function(e) e)
+		any(class(out) == "error")
+	}
+	
+    get <- function() x
+    setinverse <- function(inverse) {m <<- inverse}
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse,
+		 isnotinvertible = checkIfNotInvertible)
 }
 
 
-## Write a short comment describing this function
+
+## This function returns inverse of invertible matrixes from cache barring the first call
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+
+cacheSolve <- function(x,...)
+{
+	m <- x$getinverse()	
+	if(!is.null(m)) {
+			print("getting cached data")
+			return(m)
+	}
+	else
+	{
+		print("data not cached")
+	}
+	data <- x$get()	
+	if(!x$isnotinvertible(data))
+	{		
+		m <- solve(data)
+		x$setinverse(m)
+		m
+	}
+	else
+	{
+		print("Given matirx is not invertible")
+	}
+	
 }
